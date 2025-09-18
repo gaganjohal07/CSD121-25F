7be410151851d095b1972e3f78251a887d6c3026
Gagandeep <gaganjohal155@gmail.com>
2025-09-17 23:25:17 -0400

    initial commit

---
+++ src/main/java/lab1.java
@@ -0,0 +1,42 @@
+void main() {
+    var scanner = new Scanner(System.in);
+    var numbers = new ArrayList<Integer>();
+
+    try {
+       IO.print("How many numbers do you want to enter? ");
+        var count = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < count; i++) {
+            IO.print("Enter a number: ");
+            var input = scanner.nextLine();
+            var num = Integer.parseInt(input);
+
+            if (num < 0) {
+               IO.println("Negative number entered, skipping.");
+                continue;
+            }
+
+            numbers.add(num);
+        }
+
+        try (var writer = new FileWriter("output.txt")) {
+            for (var n : numbers) {
+                var result = square(n);
+               IO.println("Square of " + n + " is " + result);
+                writer.write("Square of " + n + " is " + result + "\n");
+            }
+        }
+
+        IO.println("Done! Check output.txt");
+
+    } catch (Exception e) {
+        IO.println("An error occurred: " + e.getMessage());
+    }
+
+    scanner.close();
+}
+
+int square(int number) {
+    return number * number;
+}
+
