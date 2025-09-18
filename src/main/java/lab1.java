b71a3820fdcd88c39c254c695e63b4be34e5edc3
gaganjohal07 <gaganjohal155@gmail.com>
2025-09-17 23:37:45 -0400

    lab1.java

--- src/main/java/lab1.java
+++ src/main/java/lab1.java
@@ -1,25 +1,20 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> da4a7dfa5ea6115ffad84bafaf75e0f0c477345b
 void main() {
     var scanner = new Scanner(System.in);
     List<Integer> numbers = new ArrayList<>();
 
     try {
-<<<<<<< HEAD
+
         IO.print("How many numbers do you want to enter? ");
         int count = Integer.parseInt(scanner.nextLine());
 
         for (int i = 0; i < count; i++) {
           IO.print("Enter a number: ");
-=======
+
         IO.println("How many numbers do you want to enter? ");
         int count = Integer.parseInt(scanner.nextLine());
 
         for (int i = 0; i < count; i++) {
             IO.println("Please enter a number: ");
->>>>>>> da4a7dfa5ea6115ffad84bafaf75e0f0c477345b
             int num = Integer.parseInt(scanner.nextLine());
 
             if (num < 0) {
@@ -30,10 +25,6 @@ void main() {
             numbers.add(num);
         }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> da4a7dfa5ea6115ffad84bafaf75e0f0c477345b
         try (var writer = new FileWriter("output.txt")) {
             for (int n : numbers) {
                 int result = square(n);
@@ -51,7 +42,7 @@ void main() {
     scanner.close();
 }
 
-<<<<<<< HEAD
+
 int square(int number) {
     return number * number;
 }
@@ -62,4 +53,3 @@ int square(int number) {
     return number * number;
 }
 
->>>>>>> da4a7dfa5ea6115ffad84bafaf75e0f0c477345b
